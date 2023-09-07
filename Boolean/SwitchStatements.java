ec99204e5348ca5324a378c222dfdab8d26d0a09
goodnews <goodnewsj62@gmail.com>
2023-09-07 01:30:24 +0100

    new java lessons on booleans and conditions

--- Boolean/SwitchStatements.java
+++ Boolean/SwitchStatements.java
@@ -29,25 +29,39 @@ public class SwitchStatements {
         int humidity = 65;
         // Determine the comfort level based on both temperature and humidity (e.g., too hot, too cold, comfortable)
         // Decide whether to use if-else or switch
+        if(temperature >= 80  && humidity >=60){
+            System.out.println("too hot and too humid\n");
+        }else if(temperature >=80){
+            System.out.println("It's too cold and humid\n");
+        }else if (temperature <= 60){
+            System.out.println("It's too cold\n");
+        }else{
+            System.out.println("It's comfortable\n");
+        }
 
         // Section 5: Age and income
         int age = 25;
         int income = 50000;
         // Determine eligibility for a specific credit card based on age and income
         // Decide whether to use if-else or switch
+        if(age >=18 && age <= 60 && income >= 3000){
+            System.out.println("You are eligible forthe credit card");
+        }else{
+            System.out.println("You are not eligible for a credit card ");
+        }
 
         // Section 1: Traffic light colors
-        String lightColor = "green";
+        // String lightColor = "green";
         // Determine what action to take based on the traffic light color (e.g., green, yellow, red)
         // Decide whether to use if-else or switch
 
         // Section 2: Exam grade
-        int grade = 85;
+        // int grade = 85;
         // Determine the letter grade based on the numeric grade (e.g., A, B, C, D, F)
         // Decide whether to use if-else or switch
 
         // Section 3: Browser type
-        String browser = "Chrome";
+        // String browser = "Chrome";
         // Check if the browser is one of the following: Chrome, Firefox, Safari, Edge, or Opera
         // Decide whether to use if-else or switch
         
