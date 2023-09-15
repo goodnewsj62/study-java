f65c6c6b947d01975f9a0cf56fc1954e11462432
goodnews <goodnewsj62@gmail.com>
2023-09-15 06:20:01 +0100

    higher order

---
+++ lamda/workbook_13.2/Main.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Double> prices = Arrays.asList(55.99, 33.99, 88.99, 11.49, 55.99, 111.99, 77.99);
+
+        prices.stream()
+            .sorted(
+                (left, right) -> right.compareTo(left)
+            ).forEach(
+                price ->  {
+                    System.out.println("\nPrices\n------"  );
+                    System.out.println(price);
+                }
+            );
+
+        
+
+
+        List<String> books = Arrays.asList(
+            "To Kill a Mockingbird",
+            "The Great Gatsby",
+            "Pride and Prejudice",
+            "The Catcher in the Rye",
+            "The Alchemist",
+            "One Hundred Years of Solitude",
+            "Moby-Dick",
+            "The Brothers Karamazov",
+            "The Lord of the Rings",
+            "The Picture of Dorian Gray"
+        );
+        
+        books.sort( (x,y) -> x.compareTo(y) );
+        System.out.println("\nLibrary\n--------");
+        books.forEach( b -> System.out.println(b));
+
+        // TODO
+
+    }
+}
