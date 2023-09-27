ae654df1ea23b61f374c8d96c00cc619b2d6c436
goodnews <goodnewsj62@gmail.com>
2023-09-27 05:07:37 +0100

    java interfaces

---
+++ Jinterfaces/service/CreditService.java
@@ -0,0 +1,16 @@
+package service;
+
+import repository.AccountRepository;
+
+public class CreditService {
+
+    AccountRepository accountRepository;
+
+
+
+    public CreditService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+
+}
